421cfef1656d19c7359d67d5abaffc070a22099f
Madhu Kumar D S <madhudskumar@live.in>
2017-01-09 18:00:14 +0530

    catch retry

--- src-server/example13.js
+++ src-server/example13.js
@@ -3,3 +3,24 @@ import {
     createSubscriber
 } from './lib/util';
 
+// Rx.Observable.concat(
+//     Rx.Observable.of(42),
+//     Rx.Observable.throw(new Error("blah")),
+//     Rx.Observable.of(10)
+// ).subscribe(createSubscriber("concat"));
+
+
+function getApi(){
+    return new Rx.Observable(observer => {
+        console.log("get Api");
+        setTimeout(function() {
+            observer.error(new Error());
+        }, 1000);
+    })
+}
+
+getApi()
+    .retry(3)
+    .catch(error => Rx.Observable.of(error))
+    .do(() => console.log("thing"))
+    .subscribe(createSubscriber("api"))
