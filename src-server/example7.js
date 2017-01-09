1c582ef0607ea0509737494c66f821b2168d2ab4
Madhu Kumar D S <madhudskumar@live.in>
2017-01-09 07:00:47 +0530

    concat merge

--- src-server/example7.js
+++ src-server/example7.js
@@ -3,23 +3,23 @@ import {
     createSubscriber
 } from './lib/util';
 
-// Rx.Observable.interval(1000)
-//     .merge(Rx.Observable.interval(500))
-//     .take(5)
-//     .subscribe(createSubscriber("merge"))
+Rx.Observable.interval(1000)
+    .merge(Rx.Observable.interval(500))
+    .take(5)
+    .subscribe(createSubscriber("merge"))
 
-// Rx.Observable.merge(
-//     Rx.Observable.interval(1000)
-//         .map(i => `${i} seconds`),
-//     Rx.Observable.interval(500)
-//         .map(i => `${i} half seconds`))
-//     .take(10)
-//     .subscribe(createSubscriber("merge2"));
+Rx.Observable.merge(
+    Rx.Observable.interval(1000)
+        .map(i => `${i} seconds`),
+    Rx.Observable.interval(500)
+        .map(i => `${i} half seconds`))
+    .take(10)
+    .subscribe(createSubscriber("merge2"));
 
-// Rx.Observable.range(1, 5)
-//     .take(3)
-//     .concat(Rx.Observable.range(10,3))
-//     .subscribe(createSubscriber("concat"))
+Rx.Observable.range(1, 5)
+    .take(3)
+    .concat(Rx.Observable.range(10,3))
+    .subscribe(createSubscriber("concat"))
 
 Rx.Observable.concat(
     Rx.Observable.interval(1000)
